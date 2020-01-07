ff202591e9ac5b2d1b12301e1f6221ba1276478a
SeungHyunMin <asdf014563@naver.com>
2020-01-07 14:59:54 +0900

    Create Github class

---
+++ app/src/main/java/com/seunghyun/dailycommit/Github.kt
@@ -0,0 +1,29 @@
+package com.seunghyun.dailycommit
+
+import android.annotation.SuppressLint
+import androidx.annotation.WorkerThread
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val BASE_URL = "https://github.com/"
+
+@WorkerThread
+class Github(private val userName: String) {
+    fun getTodayCommitCount(): Int {
+        val element = getProfilePage().select("rect").last()
+        val dateString = element.attr("data-date")
+        if (!isToday(dateString)) return 0
+        return element.attr("data-count").toInt()
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    internal fun isToday(dateString: String, dateFormat: String = "yyyy-MM-dd"): Boolean {
+        val today = Calendar.getInstance()
+        val anotherDay = Calendar.getInstance().apply { time = SimpleDateFormat(dateFormat).parse(dateString)!! }
+        return today.get(Calendar.DAY_OF_YEAR) == anotherDay.get(Calendar.DAY_OF_YEAR)
+    }
+
+    private fun getProfilePage(): Document = Jsoup.connect("$BASE_URL$userName").get()
+}
---
+++ app/src/test/java/com/seunghyun/dailycommit/GithubTest.kt
@@ -0,0 +1,36 @@
+package com.seunghyun.dailycommit
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class GithubTest {
+    private val github = Github("MinSeungHyun")
+
+    @Test
+    fun isTodayTrue() {
+        val today = SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().time)
+        assertTrue(github.isToday(today))
+    }
+
+    @Test
+    fun isTodayFalse() {
+        assertFalse(github.isToday("1234-12-12"))
+    }
+
+    @Test
+    fun isTodayError() {
+        assertThrows(ParseException::class.java) {
+            github.isToday("")
+        }
+    }
+
+    @Test
+    fun getTodayCommitCountTest() {
+        assertDoesNotThrow {
+            github.getTodayCommitCount()
+        }
+    }
+}
